d3e2ef52874e00b31d09bf9024dc99725ff9b5ba
eznrr <eznr527@sempreceub.com>
2024-08-12 10:39:58 -0300

    adicionando funcao de submit

---
+++ script.js
@@ -0,0 +1,15 @@
+let btnSubmit = document.getElementById("btn-submit");
+
+btnSubmit.addEventListener("click", submit);
+
+function submit() {
+    let nameElement = document.getElementById("nome").value;
+    let idadeElement = document.getElementById("idade").value;
+    let emailElement = document.getElementById("email").value;
+
+    console.log(nameElement);
+    console.log(idadeElement);
+    console.log(emailElement);
+
+    alert("funcao submit");
+}
