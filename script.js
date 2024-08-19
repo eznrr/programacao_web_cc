1e7528d045dfa90f5d8d72883e9a1dff0239b4b5
eznrr <eznr527@sempreceub.com>
2024-08-19 09:26:33 -0300

    algumas alteracoes

--- script.js
+++ script.js
@@ -6,19 +6,30 @@ function submit() {
     let nameElement = document.getElementById("nome").value;
     let idadeElement = document.getElementById("idade").value;
     let emailElement = document.getElementById("email").value;
+    let cpfElement = document.getElementById("cpf").value;
 
     console.log(nameElement);
     console.log(idadeElement);
-    console.log(emailElement);
+    console.log(cpfElement);
+    //console.log(emailElement);
+
+    console.log(isEmailValid(emailElement))
 
     alert("funcao submit");
-}
-
-function isEmailValid (emailElement) {
-    return /[\w.-]+@[\w-]+.[a-zA-Z]+$/.test(emailElement)
 }
 
 function isIdadeValid (idade) {
     return true;
    
 }
+
+function isCpfValid (cpf) {
+    return true;
+   
+}
+
+function isEmailValid (email) {
+    const emailValidation = /^\w+(\.\w+)*@\w+(\.\w+)+$/;
+    return emailValidation.test(email)
+}
+
